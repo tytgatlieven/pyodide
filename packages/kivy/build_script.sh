99fc0b9ccf6b78d223676f0d60c2635f3e0a6e64
Lieven Tytgat <lieven.tytgat@tymeli.be>
2021-04-02 18:40:26 +0200

    Update build_script. Environment variables are NOT set!!!

--- packages/kivy/build_script.sh
+++ packages/kivy/build_script.sh
@@ -1,3 +1,5 @@
 echo Running buildscript
 export KIVY_CROSS_PLATFORM=emscripten
+export KIVY_SDL2_PATH=/src/emsdk/emsdk/upstream/emscripten/system/include/SDL/
+export KIVY_BUILD_EXAMPLES=1
 cp /src/packages/kivy/kivy.html /src/build/
