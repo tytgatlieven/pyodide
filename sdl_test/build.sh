922ef4e711a479248fe5412f19509fc4d568ca7e
Lieven Tytgat <lieven.tytgat@tymeli.be>
2021-04-22 15:15:38 +0200

    Added sdl_test SDL2 example and build script

---
+++ sdl_test/build.sh
@@ -0,0 +1,4 @@
+../emsdk/emsdk/upstream/emscripten/emcc -c sdl_2_0_sample.c -o sdl_2_0_sample.o -s USE_SDL=2
+../emsdk/emsdk/upstream/emscripten/emcc sdl_2_0_sample.o -o sdl_2_0_sample.html -s USE_SDL=2
+
+/src/bin/pyodide serve --build_dir ./
