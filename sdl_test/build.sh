07741a584cabc149e397ce4a2ae9bcbfd8c8663f
cedrichkz <cedrichkz@gmail.com>
2021-04-23 17:18:45 +0200

    copy sdl2.c to sdl2.h

--- sdl_test/build.sh
+++ sdl_test/build.sh
@@ -1,5 +1,7 @@
-#cython ../kivy/kivy/lib/sdl2.pxi
+#LOCATION = "../kivy/kivy/lib"
 
+cython ../kivy/kivy/lib/sdl2.pxi
+cp ../kivy/kivy/lib/sdl2.c ../kivy/kivy/lib/sdl2.h
 ../emsdk/emsdk/upstream/emscripten/emcc -c sdl_2_0_sample.c -o sdl_2_0_sample.o -s USE_SDL=2
 ../emsdk/emsdk/upstream/emscripten/emcc sdl_2_0_sample.o -o sdl_2_0_sample.html -s USE_SDL=2
 
