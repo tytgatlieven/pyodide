7c5e9e5838611225750fc0ea09bdeece72eea1d4
hamlet4401 <root@pop-os.localdomain>
2021-07-11 23:04:33 +0200

    Emscripten compilation

--- manual_sdl_build.sh
+++ manual_sdl_build.sh
@@ -1,12 +1,22 @@
 cd emsdk/emsdk/upstream/emscripten/cache/ports/sdl2/SDL2-version_22
+
+#sh autogen.sh
+#chmod +x configure
+#emconfigure ./configure
+#emmake make
+
+
+
 ./configure
 make
 make install
 
-cd ../../sdl2_mixer/SDL2_mixer-release-2.0.2
-./configure
-make
-make install
+#emmake make install
+#
+#cd ../../sdl2_mixer/SDL2_mixer-release-2.0.2
+#./configure
+#make
+#make install
 
 #cd ../../sdl2_ttf/SDL_ttf-38fcb695276ed794f879d5d9c5ef4e5286a5200d
 #./configure
